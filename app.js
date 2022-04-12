51d08c43ef54411cab2fd2f26d9324d771f44e85
JohniloSancho <johnilo.sancho@mobilemoney.ph>
2022-04-12 21:10:54 +0800

    test

--- app.js
+++ app.js
@@ -1,9 +1,4 @@
 var a = "asasasasasasas"
 ///asasasasaasasssassaasas
-<<<<<<< HEAD
 //sdfasdasdasdsad
-//sdsdasdasd
-=======
-//sdf
-//sdsdasdasdasdasd
->>>>>>> 85fb08bfbf3f67ce6aa5de73c422b4e12a520c68
+//sdsdasdasdasdasdasd
