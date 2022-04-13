31184222bbd6a4da13d03fdd987eae96bd6a951d
JohniloSancho <johnilo.sancho@mobilemoney.ph>
2022-04-13 21:50:39 +0800

    FIX on Wed, Apr 13, 2022  9:50:39 PM

--- auto/help.sh
+++ auto/help.sh
@@ -8,28 +8,39 @@ AUTOG is open for support and colaboration.
 Uppercase [] means the value is from 'srcvar.sh'.
 
 Command list:
-c | C           'clear'
-s | S           'git status'
-p | P           'git pull origin [BRANCH]'
-r . | R .       'git restore .'
-r 0 1...n       'git restore [file/s]'
-a . | A .       'git add .'
-a 0 1...n       'git add [file/s]'
-u . | U .       'git restore --staged .'
-u 0 1...n       'git restore --staged [file/s]'
+c | C                       'clear'
+s | S                       'git status'
+p | P                       'git pull origin [BRANCH]'
+r . | R .                   'git restore .'
+r 0 1...n                   'git restore [file/s]'
+a . | A .                   'git add .'
+a 0 1...n                   'git add [file/s]'
+u . | U .                   'git restore --staged .'
+u 0 1...n                   'git restore --staged [file/s]'
 
-q [text]        'git pull origin [BRANCH]'
-                'git commit -m [text]'
-                'git push origin [MYBRANCH]'
+q [text]                    'git pull origin [BRANCH]'
+                            'git commit -m [text]'
+                            'git push origin [MYBRANCH]'
 
-push            'git pull origin [BRANCH]'
-                'git add .'
-                'git commit -m [MESSAGE]'
-                'git push origin [MYBRANCH]'
+push [0 1...n] [message]    'git pull origin [BRANCH]'
+                            'git add .'
+                            'git commit -m [MESSAGE]'
+                            'git push origin [MYBRANCH]'
+                            [file index] is optional
+                            [message] is optional
+                            \033[1;31m!! If using [file index] make sure to execute 's' first!!\033[0m
+                            if there are other files that was staged aside
+                            from the selected [file index]
+                            it will be pushed also.
 
-run             '[RUN]' optional
 
-d               '[PUTTY]' optional
-                '[TXT]' optional
 
-done            after the 'd' command only."
+run                         '[RUN]' optional
+
+d                           '[PUTTY]' optional
+                            '[TXT]' optional
+
+done                        After the 'd' command only.
+
+Authors:
+John, Engr. Ryan V.M.\n\n"
